7f2bf3c3324c1680a4c7d4d06f6ca2eaec7baa3d
i8abyte <imransalim08@gmail.com>
2019-09-26 18:37:33 +1200

    Clear remaining textbox after clicking 'Clear completed tasks'

--- to-do-list-functionality.js
+++ to-do-list-functionality.js
@@ -5,22 +5,23 @@ index = 1;
 function addItem() {
     var chckbx = document.createElement("INPUT");
     chckbx.setAttribute("type", "checkbox");
-    // chckbx.setAttribute("class", "item"+index)
+    chckbx.setAttribute("class", "item"+index)
     document.body.appendChild(chckbx);
 
     var txtbx = document.createElement("INPUT");
     txtbx.setAttribute("type", "text");
-    // document.setAttribute("class", "item"+index)
+    txtbx.setAttribute("class", "item"+index)
     document.body.appendChild(txtbx);
 
     var br = document.createElement("BR")
     document.body.appendChild(br);
 
+    index += 1;
     inputCounter += 2;
     brCounter += 1;
 }
 
-function removeItem() {
+function removeCheckedItems() {
     var elmntBr = document.getElementsByTagName("BR")[brCounter];
     elmntBr.remove();
     brCounter -= 1;
@@ -30,4 +31,20 @@ function removeItem() {
     elmntChckBx.remove();
     elmntTxtBx.remove();
     inputCounter -= 2;
+}
+
+function removeAllItems() {
+    while (brCounter > 0) {
+        var elmntBr = document.getElementsByTagName("BR")[brCounter];
+        elmntBr.remove();
+        brCounter -= 1;
+    }
+
+    while (inputCounter > 0) {
+        var elmntChckBx = document.getElementsByTagName("INPUT")[inputCounter - 1];
+        var elmntTxtBx = document.getElementsByTagName("INPUT")[inputCounter];
+        elmntChckBx.remove();
+        elmntTxtBx.remove();
+        inputCounter -= 2;
+    }
 }
