27e62df076ab675032a1b4b506b9e239bcbf7f8d
i8abyte <imransalim08@gmail.com>
2019-10-02 11:50:23 +1300

    Throw away 'removeCheckedItems()' code

--- to-do-list-functionality.js
+++ to-do-list-functionality.js
@@ -25,44 +25,9 @@ function addItem() {
 }
 
 function removeCheckedItems() {
-    // var elmntBr = document.getElementsByTagName("BR")[brCounter];
-    // elmntBr.remove();
-    // brCounter -= 1;
 
-    // var elmntChckBx = document.getElementsByTagName("INPUT")[inputCounter + 1];
-    // var elmntTxtBx = document.getElementsByTagName("INPUT")[inputCounter];
-    // elmntChckBx.remove();
-    // elmntTxtBx.remove();
-    // inputCounter -= 2;
-
-    // index -= 1;
-
-    var tempIndex = brCounter;
-    var itemCount = 0;
-    var textValues = []
-    while (tempIndex >= 0) {
-        var elmntChckBx = document.getElementById("checkbox"+tempIndex);
-        if (elmntChckBx.checked) {
-            var elmntTxtBx = document.getElementById("textbox"+tempIndex);
-            textValues.push(elmntTxtBx.value);
-            // alert(textValues[itemCount]);
-            itemCount += 1;
-        }
-        tempIndex -= 1
-    }
-    removeAllItems();
-    textValues.reverse();
-    // alert(brCounter);
-    // alert(inputCounter);
-    for (var i = 0; i < itemCount; i++) {
-        alert(i);
-        alert(textValues[i]);
-    }
-    // alert(itemCount);
 }
 
-// change this function to use element ids instead of tag names
-// solution on the line above this one does not work
 function removeAllItems() {
     while (brCounter >= 0) {
         var elmntBr = document.getElementsByTagName("BR")[brCounter];
@@ -71,10 +36,10 @@ function removeAllItems() {
     }
 
     while (inputCounter >= 0) {
-        var elmntChckBx = document.getElementsByTagName("INPUT")[inputCounter + 1];
-        var elmntTxtBx = document.getElementsByTagName("INPUT")[inputCounter];
-        elmntChckBx.remove();
-        elmntTxtBx.remove();
+        var elmntChckbx = document.getElementsByTagName("INPUT")[inputCounter + 1];
+        var elmntTxtbx = document.getElementsByTagName("INPUT")[inputCounter];
+        elmntChckbx.remove();
+        elmntTxtbx.remove();
         inputCounter -= 2;
     }
 
